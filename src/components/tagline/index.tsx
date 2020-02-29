02ed33c77552ed07f75fa67f976b1012984075ec
Hashed <z-donnelly@hvcc.edu>
2020-02-28 20:22:48 -0500

    finished off huge middle section and is now responsive on all screens

--- src/components/tagline/index.tsx
+++ src/components/tagline/index.tsx
@@ -41,9 +41,23 @@ const CallToAction = () => {
         </div>
       </React.Suspense>
       <LazyLoad once>
-        <div className={styles.inOnePlaceImg} />
-        <h3 className={styles.inOnePlaceHeader}>All in One Place</h3>
-        <h5 className={styles.imageHeader}>Available on any Device!</h5>
+        <section className={styles.inOnePlaceSection}>
+          <div className={styles.inOnePlaceImg} />
+          <h3 className={styles.inOnePlaceHeader}>All in One Place</h3>
+          <h5 className={styles.imageHeader}>Available on any Device!</h5>
+          <h5 className={styles.subHeader}>Save recipes from any website or blog.</h5>
+          <p className={styles.subText} style={{ marginBottom: '3rem' }}>
+            Easily grab that perfect recipe from any website or blog, and leave the rest to us.
+            RecipeBook's recipe saver works with any website in English, and many that aren't in
+            English.
+          </p>
+          <h5 className={styles.subHeader}>Your recipes are always at your fingertips.</h5>
+          <p className={styles.subText} style={{ marginBottom: '9rem' }}>
+            With Android, iOS and web apps, we've got you covered. Desktop, tablet and phone. Online
+            and offline. In the kitchen, in the grocery store, at the office -- wherever you need
+            them.
+          </p>
+        </section>
       </LazyLoad>
       <div className={styles.imageGroup}>
         <LazyLoad once>
