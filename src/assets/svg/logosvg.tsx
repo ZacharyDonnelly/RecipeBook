353a5fee15b50436feca695d77607a6df0502ccf
Hashed <z-donnelly@hvcc.edu>
2020-03-01 04:22:04 -0500

    finished login/signup forms, still need performance help and different color scheme, MVP finished though

---
+++ src/assets/svg/logosvg.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+
+const LogoSvg: React.FunctionComponent = () => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 149.57 115.18"
+        style={{ marginBottom: 5 }}
+        height="70px"
+        width="70px">
+        <line
+          x1="38.15"
+          y1="112.61"
+          x2="148.07"
+          y2="112.61"
+          fill="none"
+          stroke="#f7941d"
+          strokeMiterlimit="10"
+          strokeWidth="5.15"
+        />
+        <line
+          x1="38.15"
+          y1="112.61"
+          x2="88.69"
+          y2="1.2"
+          fill="none"
+          stroke="#f7941d"
+          strokeMiterlimit="10"
+          strokeWidth="5.15"
+        />
+        <line
+          x1="147.29"
+          y1="112.61"
+          x2="88.69"
+          y2="1.2"
+          fill="none"
+          stroke="#f7941d"
+          strokeMiterlimit="10"
+          strokeWidth="5.15"
+        />
+        <line
+          x1="2.28"
+          y1="112.61"
+          x2="112.19"
+          y2="112.61"
+          fill="none"
+          stroke="#231f20"
+          strokeMiterlimit="10"
+          strokeWidth="5.15"
+        />
+        <line
+          x1="2.28"
+          y1="112.61"
+          x2="52.82"
+          y2="1.2"
+          fill="none"
+          stroke="#231f20"
+          strokeMiterlimit="10"
+          strokeWidth="5.15"
+        />
+        <line
+          x1="111.42"
+          y1="112.61"
+          x2="52.82"
+          y2="1.2"
+          fill="none"
+          stroke="#231f20"
+          strokeMiterlimit="10"
+          strokeWidth="5.15"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default LogoSvg;
