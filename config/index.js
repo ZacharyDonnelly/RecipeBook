03d79af48cecf2fcbac82a30bec32c81083f996e
Hashed <z-donnelly@hvcc.edu>
2020-03-25 19:50:17 -0400

    ughhhh

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 export default {
-  [true]: 'http://localhost:3006',
+  [true]: 'https://http://ec2-34-207-73-14.compute-1.amazonaws.com:3006',
   [env.production === true]: 'https://http://ec2-34-207-73-14.compute-1.amazonaws.com:3006',
 }.true;
