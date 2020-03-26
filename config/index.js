b26cfb53bd0191cb6a567ab7aab8bac62c8f4a3b
Hashed <z-donnelly@hvcc.edu>
2020-03-25 20:08:22 -0400

    jhsdfjklsdjfkjsdkf fuck me

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@
 //   [env.production === true]: 'https://http://ec2-34-207-73-14.compute-1.amazonaws.com:3006',
 // }.true;
 
-const config = 'https://http://ec2-34-207-73-14.compute-1.amazonaws.com:3006';
+const config = 'http://ec2-34-207-73-14.compute-1.amazonaws.com:3006';
 
 export default config;
