2ebee78b863421c26b176ef8aeed7ec70a63be8d
Zach Donnelly <54864201+parxyHash@users.noreply.github.com>
2020-04-02 02:47:09 -0400

    Fixing config and removing comment

--- config/index.js
+++ config/index.js
@@ -1,8 +1,6 @@
-// export default {
-//   [true]: 'https://http://ec2-34-207-73-14.compute-1.amazonaws.com:3006',
-//   [env.production === true]: 'https://http://ec2-34-207-73-14.compute-1.amazonaws.com:3006',
-// }.true;
-
-const config = 'http://ec2-34-207-73-14.compute-1.amazonaws.com:3006';
+export default {
+  [true]: 'http://ec2-34-207-73-14.compute-1.amazonaws.com:3006',
+  [env.production === true]: 'http://ec2-34-207-73-14.compute-1.amazonaws.com:3006',
+}.true
 
 export default config;
