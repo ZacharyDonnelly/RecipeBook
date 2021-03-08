898d8af4a8a971d7bbce5bd0fe3e1df835e7cd5d
Zach Donnelly <54864201+parxyHash@users.noreply.github.com>
2021-03-07 23:09:13 -0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,3 @@
 export default {
   [true]: 'https://ec2-18-188-224-237.us-east-2.compute.amazonaws.com:3006',
-  [env.production === true]: 'https://ec2-18-188-224-237.us-east-2.compute.amazonaws.com:3006',
 }.true
